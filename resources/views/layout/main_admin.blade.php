1150e9852b5b5693404e79f843cf8b99e0022948
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-09-17 17:20:38 +0700

    test

--- resources/views/layout/main_admin.blade.php
+++ resources/views/layout/main_admin.blade.php
@@ -12,7 +12,8 @@
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
     <!-- Bootstrap CSS -->
-    <link href="{{ asset('public/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('public/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <title>App</title>
   </head>
@@ -32,10 +33,11 @@
 
   
     <!-- JS Bootstrap -->
-    <script src="{{ asset('public/bootstrap/js/bootstrap.bundle.min.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    {{-- <script src="{{ asset('public/bootstrap/js/bootstrap.bundle.min.js') }}" defer></script> --}}
     
     {{-- Jquery --}}
-    <script src="{{ asset('/public/JS/javascript.js') }}" defer></script>
+    {{-- <script src="{{ asset('/public/JS/javascript.js') }}" defer></script> --}}
 
     {{-- Content --}} 
     @yield('script')
